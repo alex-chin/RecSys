f7073afeca957619d4c9e6031b78b8eb26d4c37b
Александр <k142@mail.ru>
2022-02-22 17:46:15 +0300

    home03 beta

--- lessons/les03/metrics.py
+++ lessons/les03/metrics.py
@@ -4,14 +4,17 @@ Metrics
 """
 import numpy as np
 
+
 def hit_rate(recommended_list, bought_list):
     bought_list = np.array(bought_list)
     recommended_list = np.array(recommended_list)
     flags = np.isin(bought_list, recommended_list)
     return (flags.sum() > 0) * 1
 
+
 def hit_rate_at_k(recommended_list, bought_list, k=5):
     return hit_rate(recommended_list[:k], bought_list)
+
 
 def precision(recommended_list, bought_list):
     bought_list = np.array(bought_list)
@@ -19,14 +22,17 @@ def precision(recommended_list, bought_list):
     flags = np.isin(bought_list, recommended_list)
     return flags.sum() / len(recommended_list)
 
+
 def precision_at_k(recommended_list, bought_list, k=5):
     return precision(recommended_list[:k], bought_list)
+
 
 def money_precision_at_k(recommended_list, bought_list, prices_recommended, k=5):
     recommended_list = np.array(recommended_list)[:k]
     prices_recommended = np.array(prices_recommended)[:k]
     flags = np.isin(recommended_list, bought_list)
     return np.dot(flags, prices_recommended).sum() / prices_recommended.sum()
+
 
 def recall(recommended_list, bought_list):
     bought_list = np.array(bought_list)
@@ -58,8 +64,6 @@ def ap_k(recommended_list, bought_list, k=5):
         return 0
     amount_relevant = len(relevant_indexes)
 
-
     sum_ = sum(
         [precision_at_k(recommended_list, bought_list, k=index_relevant + 1) for index_relevant in relevant_indexes])
     return sum_ / amount_relevant
-
